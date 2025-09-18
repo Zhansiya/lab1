46efe27d1eb34a03a11191bbdd0407f65b28a1f0
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-18 19:56:57 +0500

    Example 10

--- string.py
+++ string.py
@@ -28,3 +28,10 @@ for x in "banana":
 
 a = "Hello, World!"
 print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
